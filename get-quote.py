9ca8f455c71a374de39f6859092562b09001bc37
obroad23 <o.broad@ucl.ac.uk>
2021-11-24 18:17:49 +0000

    upgrade quote bot to choose random quote

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+import random
 def robert():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  n = len(quotes)-1
+  rn = random.randint(0,n)
+  print(rn,quotes[rn])
 
 if __name__== "__main__":
   robert()
